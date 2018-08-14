5fe820814700b128f60b9bb36e0c45feff5cb368
Gilberts Ahumada Cabrera <gilbertsahumada@gmail.com>
2018-08-14 17:08:22 +1200

    transform data api

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
 import PropTypes from 'prop-types'
 
-
-class WeatherData extends Component {
-    
-    render = ({ data }) => {
+const WeatherData  = ({ data }) =>  {     
         const { temperature, weatherState, humidity, wind } = data;
-        return (
-            <div>
+        return (<div>
                 <WeatherTemperature temperature={temperature} weatherState={weatherState} />
                 <WeatherExtraInfo humidity={humidity} wind={wind} />
-            </div>
-        )
-    };
+            </div>);
 };
 
 WeatherData.propTypes = {
@@ -27,3 +21,42 @@ WeatherData.propTypes = {
 }
 
 export default WeatherData;
+
+/*
+const location = "London"
+const api_key = "0a73711b0e226e5ae343f3bb397e6b24";
+const url_api_weather = `https://samples.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+*/
+
+
+   /*
+  getWeatherState = weather => {
+      return SNOW;
+  }
+
+  getData = weather_data => {
+      const { humidity, temp } = weather_data.main;
+      const { speed } = weather_data.wind;
+      const weatherState = this.getWeatherState(this.weather);
+
+      const data = {
+          humidity,
+          temperature: temp,
+          weatherState,
+          wind: `${speed} m/s`
+      }
+
+      return data;
+  }
+ 
+
+  handleUpdateClick = () => {
+      fetch(url_api_weather)
+          .then((recurso) => {
+              const data = this.getData(recurso);
+              this.setState({ data })
+              console.log(recurso)
+
+          });
+  };
+   */
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -10,19 +10,20 @@ const data1 = {
     wind: '10 m/s'
 }
 
-
 const location = "London"
-const api_key = "0a73711b0e226e5ae343f3bb397e6b24";
+const api_key = "0a73711b0e226e5ae343f3bb397e6b24"; //http://api.apixu.com/v1/current.json?key=f99bf1b937bc45888c235351181408&q=Paris
+
 const url_api_weather = `https://samples.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+
 
 class WeatherLocation extends Component {
 
-    constructor() {
+    constructor(){
         super();
         this.state = {
-            city: "Buenos Aires",
+            city: "Santiago",
             data: data1
-        };
+        }
     }
 
     getWeatherState = weather => {
@@ -30,9 +31,9 @@ class WeatherLocation extends Component {
     }
 
     getData = weather_data => {
-        const {humidity, temp} = weather_data.main;
-        const {speed} = weather_data.wind;
-        const weatherState  = this.getWeatherState(this.weather);
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(this.weather);
 
         const data = {
             humidity,
@@ -40,31 +41,28 @@ class WeatherLocation extends Component {
             weatherState,
             wind: `${speed} m/s`
         }
-
         return data;
     }
 
-
-
     handleUpdateClick = () => {
         fetch(url_api_weather)
-            .then((recurso) => {
-                const data = this.getData(recurso);
-                this.setState({ data })
-                console.log(recurso)
-
+            .then((response) => {
+                return response.json()
             })
-
-
-    }
-
+            .then(obj => {
+                const data =  this.getData(obj);
+                this.setState({ data })
+                console.log(data)
+            });
+    };
+   
     render = () => {
         const { city, data } = this.state;
         return (<div>
-            <Location city={city} />
-            <WeatherData data={data} />
+            <Location city={ city } />
+            <WeatherData data={ data } />
             <button onClick={this.handleUpdateClick}>Actualizar</button>
-        </div>)
+        </div>);
     };
 }
 
